83109b879a6b8c8329f104704e655d912fc91a00
sheilaschlickmann <61628827+sheilaschlickmann@users.noreply.github.com>
2020-05-24 19:14:03 -0300

    Webpack server

---
+++ Principal.js
@@ -0,0 +1,10 @@
+
+import ClasseUsuario from './Functions';
+console.log(ClasseUsuario.info());
+
+import { idade } from './Functions';
+console.log(idade);
+
+import defaultUsuario, { idade as IdadeUsuario } from './Functions';
+
+      
